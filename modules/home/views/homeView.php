10875a767b4c69a019303eaa5a70a6db37e4f88e
Tín <Trungtin0701200@gmail.com>
2025-07-22 23:33:16 +0700

    Nếu chưa đăng nhập thì hiện 2 nút sign in và sign up, nếu đăng nhập rồi thì hiện username và hiện nút log out

--- modules/home/views/homeView.php
+++ modules/home/views/homeView.php
@@ -33,6 +33,7 @@
 
     <body>
         <div class="container">
+
             <div class="header">
                 <img src="<?php echo $config['base_url']; ?>public/resources/images/Rectangle93.png"
                     alt="Liverpool Background" class="background" />
@@ -58,19 +59,45 @@
                             d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20l44 0 0 44c0 11 9 20 20 20s20-9 20-20l0-44 44 0c11 0 20-9 20-20s-9-20-20-20l-44 0 0-44c0-11-9-20-20-20s-20 9-20 20l0 44-44 0c-11 0-20 9-20 20z" />
                     </svg>
                 </div>
+
                 <div class="khoiuser">
+
+                    <?php if (isset($_SESSION['is_login']) && $_SESSION['is_login'] === true): ?>
+                    <!-- ĐÃ ĐĂNG NHẬP -->
+
                     <div class="login user">
                         <img src="<?php echo $config['base_url']; ?>public/resources/images/Login.png" alt="login"
                             class="img" />
-                        <a href="" class="desc">Login</a>
+                        <span class="desc"><?php echo htmlspecialchars($_SESSION['user_login']); ?></span>
                     </div>
                     <div class="gach"></div>
                     <div class="signup user">
                         <img src="<?php echo $config['base_url']; ?>public/resources/images/signup.png" alt="signup"
                             class="img" />
-                        <a href="" class="desc">Sign Up</a>
+                        <a href="<?php echo $config['base_url']; ?>?mod=auth&controller=auth&action=logout"
+                            class="desc">Log Out</a>
                     </div>
+
+                    <?php else: ?>
+                    <!-- CHƯA ĐĂNG NHẬP -->
+
+                    <div class="login user">
+                        <img src="<?php echo $config['base_url']; ?>public/resources/images/Login.png" alt="login"
+                            class="img" />
+                        <a href="<?php echo $config['base_url']; ?>?mod=auth&controller=auth&action=sign_in"
+                            class="desc">Sign in</a>
+                    </div>
+                    <div class="gach"></div>
+                    <div class="signup user">
+                        <img src="<?php echo $config['base_url']; ?>public/resources/images/signup.png" alt="signup"
+                            class="img" />
+                        <a href="<?php echo $config['base_url']; ?>?mod=auth&controller=auth&action=sign_up"
+                            class="desc">Sign Up</a>
+                    </div>
+
+                    <?php endif; ?>
                 </div>
+
             </div>
 
 
